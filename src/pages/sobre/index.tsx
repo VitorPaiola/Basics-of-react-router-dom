bdfc87e77e838db6590a96f90c8fd007b056c670
Vitor Paiola <vitinho.souza1234@gmail.com>
2023-08-01 10:26:23 -0300

    first commit

---
+++ src/pages/sobre/index.tsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+
+export function Sobre() {
+    return (
+        <div>
+            <h1>Bem-vindo a página sobre</h1>
+            <Link to="/">Home</Link>
+        </div>
+    )
+}
